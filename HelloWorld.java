f5c728024aea2025a108556840026d3b7b656ea0
Vatsal Patani <vatsalpatani@gmail.com>
2023-02-08 20:17:14 +0530

    java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld , welcome to JAVA");
+    }
+}
+// output : HelloWorld , welcome to JAVA
