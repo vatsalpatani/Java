2b7e6f65f6b66714b0bce2e47f0867640480fabd
Vatsal Patani <vatsalpatani@gmail.com>
2023-02-08 23:05:30 +0530

    java

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        var a = 10;
+        var b = 20;
+        var c = a + b;
+        System.out.println("Addition is " + c);
+    }
+}
+// output : Addition is 30
---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        int c = a - b;
+        System.out.println("Subtraction is " + c);
+    }
+}
+// output : Subtraction is 10
