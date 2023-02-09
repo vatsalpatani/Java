3d0ad99187e332ed84cc257ed2a80c2a2b00e32b
Vatsal Patani <vatsalpatani@gmail.com>
2023-02-09 23:28:54 +0530

    java

---
+++ doWhileLoop.java
@@ -0,0 +1,16 @@
+public class doWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }while(i <= 5);
+    }
+}
+/* output :
+1
+2
+3
+4
+5
+*/
---
+++ forLoop.java
@@ -0,0 +1,14 @@
+public class forLoop {
+    public static void main(String[] args) {
+        for(int i = 1 ; i <= 5 ; i++){
+            System.out.println(i);
+        }
+    }
+}
+/* output :
+1
+2
+3
+4
+5
+*/
